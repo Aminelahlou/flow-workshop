ab68d3aef3162ccf12cface700d6286282b55569
Amine Lahlou <amine.lahlou@fluo.com>
2018-02-05 21:26:46 +0100

    feat(fifth-exercice) add solution to fifth exercice

--- 5.js
+++ 5.js
@@ -8,8 +8,7 @@ const points: Point2d[] = [
   { x: 1, y: 2},
   { x: -1, y: 2}
 ]
-const numbers = [2, 3, 4];
 function testFunc(point: Point2d):bool {
   return point.x > 0 && point.y > 0;
 }
-_.some(numbers, testFunc);
+_.some(points, testFunc);
