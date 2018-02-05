a1f5208dbde1128345fd2c3ce4c8c3909e5720db
Amine Lahlou <amine.lahlou@fluo.com>
2018-02-05 20:35:58 +0100

    feat(fourth-exercice): add fourth exercice

---
+++ 4.js
@@ -0,0 +1,28 @@
+// @flow
+type User = {
+  name: string,
+  age: number
+}
+async function getUsers(): Promise<User[]> {
+  const user1 = {
+    name: "Jean",
+    age: 30
+  }
+  const user2 = {
+    name: "Jeanne",
+    age: 42
+  }
+  const users = [
+    user1,
+    user2
+  ]
+  return users;
+}
+async () => {
+  getUsers().then(res => {
+    const users: User[] = res;
+  });
+}
+async () => {
+  const users: User[] = getUsers()
+}
