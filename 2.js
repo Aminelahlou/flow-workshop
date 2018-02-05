639142da17237ba261d132fdf2664367e24dab09
Amine Lahlou <amine.lahlou@fluo.com>
2018-02-05 19:57:56 +0100

    feat(first-exercice): add solutions to first exercice

--- 2.js
+++ 2.js
@@ -1,15 +1,21 @@
 // @flow
 
-function getHouse(lotSize, materials, time) {
+type House = {
+  sqft: number,
+  cost: number,
+}
+type Months = number
+function getHouse(lotSize: number, materials: string[], time: Months): House {
+    const sqrt = lotSize / 2
     return {
         sqft: lotSize / 2,
-        cost: sqft * materials.length * time,
+        cost: sqrt * materials.length * time,
     }
 }
 
-function sellHouse(house) {
-    let salePrice = house.cost * 2
-    let taxes = house.cost * .1
+function sellHouse(house: House): number {
+    const salePrice = house.cost * 2
+    const taxes = house.cost * .1
     return salePrice - taxes
 }
 
@@ -17,9 +23,7 @@ var materials = ["boards", "nails", "paint"]
 var lotSize = 4000
 var time = 4 // time in months
 
-var house = getHouse()
+var house = getHouse(lotSize, materials, time)
 var profit = sellHouse(house)
 
 console.log(`You sold your house for ${profit}`)
-
-
