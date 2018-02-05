87c67702cd6b6a0df1957f1f602390fd5b3ed5e3
Amine Lahlou <amine.lahlou@fluo.com>
2018-02-05 20:03:07 +0100

    feat(second-exercice): add solution to second exercice

--- 1.js
+++ 1.js
@@ -1,17 +1,15 @@
 // @flow
 
-function sayHi(x) {
-    if (x) {
-        return `Hello, I am ${x} years old`
-    }
+function sayHi(x: number): string {
+  return `Hello, I am ${x} years old`
 }
 
-function takeFirstWord(words) {
+function takeFirstWord(words: string): string {
     return words.split(' ')[0].replace(',', '')
 }
 
-var message = takeFirstWord(sayHi(10))
-message2 = takeFirstWord(sayHi());
+const message = takeFirstWord(sayHi(10))
+const message2 = takeFirstWord(sayHi(12));
 
 console.log(message)
 console.log(message2)
