22595b0b5bbfc6d5752de1fe9dd89c76acf0b4d1
Amine Lahlou <amine.lahlou@fluo.com>
2018-02-05 21:23:59 +0100

    feat(fifth-exercice): complete fifth exercice

--- 5.js
+++ 5.js
@@ -1,3 +1,15 @@
 // @flow
-import express from 'express';
-const app = express();
+const _ = require('lodash');
+type Point2d = {|
+  x: number,
+  y: number
+|};
+const points: Point2d[] = [
+  { x: 1, y: 2},
+  { x: -1, y: 2}
+]
+const numbers = [2, 3, 4];
+function testFunc(point: Point2d):bool {
+  return point.x > 0 && point.y > 0;
+}
+_.some(numbers, testFunc);
